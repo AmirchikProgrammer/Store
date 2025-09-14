6b5dc2ccd8924d2f649b5b9a70213c99716bcb87
AmirchikProgrammer <amiraliyev15072014@gmail.com>
2025-09-14 09:14:04 +0400

    Update store.py
    Commits:
    1. While!
    2. Code update!

--- store.py
+++ store.py
@@ -1,37 +1,31 @@
-balance = 4000
+balance = 200
+cart = []
 
-print(f"You are a millionare (Amir), also your balance is {balance}")
+store = {"cup": 18, "trousers": 80, "shirt": 65}
+prices = {"cup": 5, "trousers": 30, "shirt": 15,}
 
-storage = {
-    'cap': 18, # 5 dollars
-    'trousers': 80, # 30 dollars
-    'shirt': 65 # 50 dollars
+while True:
+    choice = input("Type 'store' or 'exit': ")
 
-}
+    if choice == "exit":
+        print("Cart:", cart)
+        print("Balance:", balance)
+        break
 
-storage_prices = {
-    'cap_price': 5,
-    'trousers_price': 30,
-    'shirt_price': 50 
+    if choice == "store":
+        print("Store:", store)
+        print("Prices:", prices)
+        item = input("What do you want to buy? ")
 
-}
-
-
-question = input("Do you want to buy something?")
-if question == "yes":
-    print("ok")
-    print(f"Now, you have {balance} dollars")
-    print(f"storage: {storage}, prices: {storage_prices}")
-    choice_c = input("What thing do you wanna to buy?")
-    if choice_c == "cap":
-        print(f"Cap price is {storage_prices['cap_price']}")
-        balance = balance - 5
-        storage['cap'] = storage['cap'] - 1
-        print(f"your balance is {balance} cap left: {storage['cap']}")
-    elif choice_c == "trousers":
-        print(f"trousers price is {storage_prices['trousers_price']}")
-        balance = balance - 30
-        storage['trousers'] = storage['trousers'] - 1
-        print(f"your balance is {balance} cap left: {storage['cap']}")
-elif question == "no":
-    print("Command break...")
+        if item in store:
+            if store[item] > 0 and balance >= prices[item]:
+                cart.append(item)
+                balance -= prices[item]
+                store[item] -= 1
+                print("Added:", item)
+                print("Cart:", cart)
+                print("Balance:", balance)
+            else:
+                print("No items left or not enough money!")
+        else:
+            print("No such item in store!")
