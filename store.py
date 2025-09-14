505a58e411fdd2f5a23eadf1b54045eb03e212a6
AmirchikProgrammer <amiraliyev15072014@gmail.com>
2025-09-14 10:25:30 +0400

    Update store.py
    Update

--- store.py
+++ store.py
@@ -4,8 +4,10 @@ cart = []
 store = {"cup": 20, "trousers": 60, "shirt": 40}
 prices = {"cup": 50, "trousers": 200, "shirt": 150,}
 
+restoraunt = {"xingal": 20, "crab": 60, "fish": 40}
+prices1 = {"xingal": 20, "crab": 40, "fish": 30,}
 while True:
-    choice = input("Type 'store' or 'exit': ")
+    choice = input("Type 'store', 'restoraunt' or 'exit': ")
 
     if choice == "exit":
         print("Cart:", cart)
@@ -15,7 +17,7 @@ while True:
     if choice == "store":
         print("Store:", store)
         print("Prices:", prices)
-        item = input("What do you want to buy? ")
+        item = input("What do you want to buy in store? ")
 
         if item in store:
             if store[item] > 0 and balance >= prices[item]:
@@ -29,3 +31,20 @@ while True:
                 print("No items left or not enough money!")
         else:
             print("No such item in store!")
+    elif choice == "restoraunt":
+        print("Restoraunt:", restoraunt)
+        print("Prices:", prices1)
+        item = input("What do you want to buy in restoraunt? ")
+
+        if item in restoraunt:
+            if restoraunt[item] > 0 and balance >= prices1[item]:
+                cart.append(item)
+                balance -= prices1[item]
+                restoraunt[item] -= 1
+                print("Added:", item)
+                print("Cart:", cart)
+                print("Balance:", balance)
+            else:
+                print("No items left or not enough money!")
+        else:
+            print("No such item in restoraunt!")
